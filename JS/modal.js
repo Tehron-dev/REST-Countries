5653c6b308bf94313e7be3cf7b32bf90c596a4ac
behzodbek-dev <behzodnosirov2009@gmail.com>
2025-04-12 02:56:44 +0500

    Final commit inshallah

--- JS/modal.js
+++ JS/modal.js
@@ -1,18 +1,37 @@
-const elFlagImg = document.querySelector(".js-country-data-flag-img");
-const elDataCountryName = document.querySelector(".js-data-country-name");
-const elDataCountryNativename = document.querySelector(".js-data-country-nativename");
+const elFlagCountry = document.querySelector(".js-country-data-flag-img");
+const elCountryName = document.querySelector(".js-data-country-name");
+const elCountryNativeName = document.querySelector(".js-data-country-nativename");
+const elPopulation = document.querySelector(".js-country-data-population");
+const elCountryRegion = document.querySelector(".js-data-country-region");
+const elCountrySubRegion = document.querySelector(".js-data-country-sub-region");
+const elCoutnryCapital = document.querySelector(".js-data-country-capital");
+const elCountryBorders = document.querySelector(".js-data-country-borders");
+const elCountryDomain = document.querySelector(".js-country-domain");
+const elCountryCurrencie = document.querySelector(".js-country-cuurencie");
+const elCountryLanguages = document.querySelector(".js-country-languages");
 
-const country = (evt) => {
-    if(evt.target.dataset.id){
-        // window.location.href = "/html/main.html";
-        countryData(evt.target.dataset.id);
-    };
-};
+let data = []
 
-function countryData(countryId){
-    let country = countries.find(country => country.id == countryId);
-    console.log(country)
-    elFlagImg.src = country.flags.png;
-    elDataCountryName.textContent = country.name.common;
-    elDataCountryNativename.textContent = country.name.nativeName.eng.common
+async function handleCountry(){
+    let country = window.localStorage.getItem("countryName");
+    let req = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+    data = await req.json();
+    handleRenderCountry(data);
 };
+handleCountry()
+
+function handleRenderCountry(arr){
+    arr.forEach(country => {
+        elFlagCountry.src = country.flags.png ? country.flags.png : "Flag picture wasn't found";
+        elCountryName.textContent = country.name.common ? country.name.common : "Name was not found";
+        elCountryNativeName.textContent = country.name.nativeName ? Object.values(country.name.nativeName) : "Native namewas not found";
+        elPopulation.textContent = country.population ? country.population : "Country population was not found";
+        elCountryRegion.textContent = country.region ? country.region : "Region was not found";
+        elCountrySubRegion.textContent = country.subregion ? country.subregion : "Subregion was not found";
+        elCoutnryCapital.textContent = country.capital[0] ? country.capital[0] : "Capital was not found";
+        elCountryBorders.textContent = country.borders ? country.borders.join(", ") : "Borders were not found";
+        elCountryDomain.textContent = country.domain ? country.domain : "Domain was not found";
+        elCountryCurrencie.textContent = country.currencies ? Object.values(country.currencies)[0].name : "Currencie was not found";
+        elCountryLanguages.textContent = country.languages ? Object.values(country.languages) : "Languages were not found";
+    })
+}
